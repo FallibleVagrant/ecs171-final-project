20c2707faf47dcfdc9fb51c7b0d24464972bc82e
FallibleVagrant <124470389+FallibleVagrant@users.noreply.github.com>
2024-02-29 23:04:42 -0800

    Fix trim_to_unique, hopefully.

--- trim_to_unique.py
+++ trim_to_unique.py
@@ -4,7 +4,7 @@ df = pd.read_csv("spotify_songs.csv")
 
 df = df.drop(columns=['playlist_id', 'playlist_name', 'playlist_genre', 'playlist_subgenre'])
 df.drop_duplicates(subset='track_id', inplace=True)
-df.drop_duplicates(subset='track_name', inplace=True)
+df.drop_duplicates(subset=['track_name', 'track_artist', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo'], inplace=True)
 
 df = df.dropna(how='any',axis=0)
 

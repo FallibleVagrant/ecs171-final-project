6e65df8cc037bb17fcb5971422af6f1007779de8
Kevin Xu <84105756+K3V1N321@users.noreply.github.com>
2024-03-09 21:17:02 -0800

    random_forest avg acc: 0.536

--- random_forest.py
+++ random_forest.py
@@ -1,62 +1,53 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
 from sklearn.metrics import classification_report
-from sklearn.metrics import accuracy_score, precision_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from eda import clean_data
 
+def test_model(df):
+    train, test = train_test_split(df, test_size = 0.2, random_state = 20)
+    x_train = train.drop(columns = ["track_name", "track_artist", "playlist_genre"])
+    y_train = train["playlist_genre"]
+    
+    x_test = test.drop(columns = ["track_name", "track_artist", "playlist_genre"])
+    y_test = test["playlist_genre"]
 
-df = pd.read_csv("spotify_songs.csv")
+    number_estimators = [125, 155, 195]
+    min_samples_for_split = [3, 4, 5]
+    min_samples_for_leaf = [1, 2, 3]
 
-df = df.drop(columns=['playlist_id', 'playlist_name', 'playlist_subgenre'])
-df.drop_duplicates(subset='track_id', inplace=True)
-df.drop_duplicates(subset=['track_name', 'track_artist', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo'], inplace=True)
+    param_grid = dict(n_estimators = number_estimators, min_samples_split = min_samples_for_split,
+                      min_samples_leaf = min_samples_for_leaf)
+    rf = RandomForestClassifier(random_state = 20)
+    grid = GridSearchCV(estimator = rf, param_grid = param_grid, cv = 3)
+    grid.fit(x_train,y_train)
 
-df = df.dropna(how='any',axis=0)
+    print("Optimal Hyper-parameters:", grid.best_params_)
+    optimal_n_estimators = grid.best_params_["n_estimators"]
+    optimal_min_samples_split = grid.best_params_["min_samples_split"]
+    optimal_min_samples_leaf = grid.best_params_["min_samples_leaf"]
 
-scaler = MinMaxScaler(feature_range = (0,1))
-df[["loudness", "tempo", "duration_ms"]] = scaler.fit_transform(df[["loudness", "tempo", "duration_ms"]])
+    optimal_rf = RandomForestClassifier(n_estimators = optimal_n_estimators, min_samples_split = optimal_min_samples_split, min_samples_leaf = optimal_min_samples_leaf, random_state = 20)
+    optimal_rf.fit(x_train, y_train)
+    y_pred = optimal_rf.predict(x_test)
 
-df = df.reset_index(drop = True)
+    print(classification_report(y_test, y_pred))
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("Precision:", precision_score(y_test, y_pred, average = "macro"))
+    print("Recall", recall_score(y_test, y_pred, average = "macro"), "\n")
 
-df = df.drop(columns = ["instrumentalness", "track_id", "track_name", "track_artist", "track_album_id", "track_album_name", "track_album_release_date"])
+    x = df.drop(columns = ["track_name", "track_artist", "playlist_genre"])
+    y = df["playlist_genre"]
+    cross_validation = cross_validate(optimal_rf, x, y, cv = 10, scoring = ["accuracy"])
+    average_accuracy = sum(cross_validation["test_accuracy"]) / len(cross_validation["test_accuracy"])
+    print("10-fold cross validation:")
+    print("Accuracy values:", cross_validation["test_accuracy"])
+    print("Average accuracy:", average_accuracy)    
 
-labels = df["playlist_genre"].unique()
-numeric_categories = ["danceability", "energy", "loudness", "speechiness", "acousticness", "liveness", "valence", "tempo", "duration_ms"]
-for label in labels:
-    genre_group = df.loc[df['playlist_genre'] == label]
-    remove_indices = []
-    for category in numeric_categories:
-        q75 = genre_group[category].quantile(0.75)
-        q25 = genre_group[category].quantile(0.25)
-        iqr = q75 - q25
-        remove_indices = genre_group.loc[genre_group[category] >= q75 + 1.5 * iqr].index.tolist()
-        remove_indices.extend(genre_group.loc[genre_group[category] <= q25 - 1.5 * iqr].index.tolist())
-    df = df.drop(remove_indices)
-    df = df.reset_index(drop = True)
-
-x = df.drop(columns = ["playlist_genre"])
-y = df["playlist_genre"]
-
-train, test = train_test_split(df, test_size = 0.2, random_state = 20)
-x_train = train.drop(columns = ["playlist_genre"])
-y_train = train["playlist_genre"]
-
-x_test = test.drop(columns = ["playlist_genre"])
-y_test = test["playlist_genre"]
-
-# Average accuracy: 0.5311
-
-rf = RandomForestClassifier(n_estimators = 100, criterion = "gini")
-rf.fit(x_train, y_train)
-y_pred = rf.predict(x_test)
-
-
-print("Accuracy:", accuracy_score(y_test, y_pred))
-print("Precision:", precision_score(y_test, y_pred, average = "macro"))
-print(classification_report(y_test, y_pred))
-cross_validation = cross_validate(rf, x, y, cv = 10, scoring = ["accuracy"])
-average_accuracy = sum(cross_validation["test_accuracy"]) / len(cross_validation["test_accuracy"])
-print("Accuracy values:", cross_validation["test_accuracy"])
-print("Average accuracy:", average_accuracy, "\n")
+if __name__ == "__main__":
+    df = pd.read_csv("spotify_songs.csv")
+    df = clean_data(df)
+    test_model(df)

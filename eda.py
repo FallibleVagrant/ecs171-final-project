e8e461333caeea418d8b3ca718f522f2c6b9fce1
K3V1N321 <kevinxu369@yahoo.com>
2024-03-10 20:15:43 -0700

    Boxplot

--- eda.py
+++ eda.py
@@ -1,4 +1,40 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
+
+
+def graph_distributions(df):
+    fig, axes = plt.subplots(7, 2, figsize = (50, 50))
+    sns.histplot(df, x = "danceability", hue = "playlist_genre", ax = axes[0][0])
+    sns.histplot(df, x = "energy", hue = "playlist_genre", ax = axes[0][1])
+    sns.histplot(df, x = "loudness", hue = "playlist_genre", ax = axes[1][0])
+    sns.histplot(df, x = "speechiness", hue = "playlist_genre", ax = axes[1][1])
+    sns.histplot(df, x = "acousticness", hue = "playlist_genre", ax = axes[2][0])
+    sns.histplot(df, x = "instrumentalness", hue = "playlist_genre", ax = axes[2][1])
+    sns.histplot(df, x = "liveness", hue = "playlist_genre", ax = axes[3][0])
+    sns.histplot(df, x = "valence", hue = "playlist_genre", ax = axes[3][1])
+    sns.histplot(df, x = "tempo", hue = "playlist_genre", ax = axes[4][0])
+    sns.histplot(df, x = "duration_ms", hue = "playlist_genre", ax = axes[4][1])
+    sns.histplot(df, x = "track_popularity", hue = "playlist_genre", ax = axes[5][0])
+    sns.histplot(df, x = "key", hue = "playlist_genre", ax = axes[5][1])
+    sns.histplot(df, x = "mode", hue = "playlist_genre", ax = axes[6][0])
+    
+def graph_boxplots(df):
+    fig, axes = plt.subplots(7, 2, figsize = (50, 50))
+    sns.boxplot(df, x = "danceability", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[0][0])
+    sns.boxplot(df, x = "energy", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[0][1])
+    sns.boxplot(df, x = "loudness", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[1][0])
+    sns.boxplot(df, x = "speechiness", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[1][1])
+    sns.boxplot(df, x = "acousticness", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[2][0])
+    sns.boxplot(df, x = "instrumentalness", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[2][1])
+    sns.boxplot(df, x = "liveness", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[3][0])
+    sns.boxplot(df, x = "valence", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[3][1])
+    sns.boxplot(df, x = "tempo", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[4][0])
+    sns.boxplot(df, x = "duration_ms", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[4][1])
+    sns.boxplot(df, x = "track_popularity", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[5][0])
+    sns.boxplot(df, x = "key", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[5][1])
+    sns.boxplot(df, x = "mode", hue = "playlist_genre", whis = (0.20, 0.80), ax = axes[6][0])
 
 def clean_data(df):
     df = df.drop(columns=['playlist_id', 'playlist_name', 'playlist_subgenre'])
@@ -14,18 +50,23 @@ def clean_data(df):
 
     df = df.drop(columns = ["instrumentalness", "track_id", "track_album_id", "track_album_name", "track_album_release_date"])
 
+    # Remove outliers for each genre
     labels = df["playlist_genre"].unique()
     numeric_categories = ["danceability", "energy", "loudness", "speechiness", "acousticness", "liveness", "valence", "tempo", "duration_ms"]
     for label in labels:
         genre_group = df.loc[df['playlist_genre'] == label]
         remove_indices = []
         for category in numeric_categories:
-            q75 = genre_group[category].quantile(0.80)
-            q25 = genre_group[category].quantile(0.20)
-            iqr = q75 - q25
-            remove_indices = genre_group.loc[genre_group[category] >= q75 + 1.5 * iqr].index.tolist()
-            remove_indices.extend(genre_group.loc[genre_group[category] <= q25 - 1.5 * iqr].index.tolist())
+            q80 = genre_group[category].quantile(0.80)
+            q20 = genre_group[category].quantile(0.20)
+            iqr = q80 - q20
+            remove_indices = genre_group.loc[genre_group[category] >= q80 + 1.5 * iqr].index.tolist()
+            remove_indices.extend(genre_group.loc[genre_group[category] <= q20 - 1.5 * iqr].index.tolist())
         df = df.drop(remove_indices)
         df = df.reset_index(drop = True)
     
     return df
+
+df = pd.read_csv("spotify_songs.csv")
+graph_distributions(df)
+graph_boxplots(df)

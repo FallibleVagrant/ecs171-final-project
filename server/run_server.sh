0fbbe7ebaddd2680832df4763bda85b4a7a16ac5
FallibleVagrant <124470389+FallibleVagrant@users.noreply.github.com>
2024-03-13 08:50:11 -0700

    Make run_server pause after trying to gen pkl file.

--- server/run_server.sh
+++ server/run_server.sh
@@ -2,7 +2,12 @@
 if ! [ -f "random_forest.pkl" ]; then
 	echo "Could not find model; generating random_forest.pkl file by running 'python3 gen_pkl_file.py'."
 	python3 gen_pkl_file.py
-	echo "Done! Probably."
+	echo "Done! But if you're seeing error messages, you should CTRL-C and fix those."
+	for i in $(seq 1 5);
+	do
+		echo "."
+		sleep 1
+	done
 fi
 
 echo "Starting server..."

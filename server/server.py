258e642e1a02fa4d8aa0d2420844be05d44e4d87
FallibleVagrant <124470389+FallibleVagrant@users.noreply.github.com>
2024-03-12 22:37:06 -0700

    Add input mechanism for raw features to server.py.

--- server/server.py
+++ server/server.py
@@ -15,20 +15,22 @@ def index():
 
 # features looks like: [track_popularity, danceability, energy, key, loudness, mode, speechiness, acousticness, liveness, valence, tempo, duration_ms]
 def get_genre_from_features(model, features):
-    genre = "test"
+    genre = f"test {features}"
 
     #My python version is 3.9 and I am *NOT* dealing with multiple python installs,
     #so we're using if-else statements rather than match statements.
+
+    #Scratch the above, we're doing one model for now.
     if model == "random_forest":
         return genre
-    elif model == "other":
-        return genre
-    return genre
+    else:
+        raise ValueError
 
 @app.post("/")
 def receive_form_data():
     try:
         song_name = request.form["song_name"]
+
         #We have the song name; now we can do a spotify API request to figure out features.
         features = get_song_features(song_name)
         returned_song_name = features[0] + " by " + features[1]
@@ -42,6 +44,9 @@ def receive_form_data():
         returned_song_name = "Song found: " + returned_song_name
         returned_genre = "Predicted genre: " + returned_genre
 
+        #Also find the closest songs to recommend.
+        #TODO...
+
         output_lines = [returned_song_name, returned_genre]
 
         for line in output_lines:
@@ -52,10 +57,20 @@ def receive_form_data():
     #If song name isn't provided, a KeyError will be raised,
     #and we'll try to look for features instead.
     except KeyError as e:
-        test_feature_1 = request.form["test_feature_1"] 
-        test_feature_2 = request.form["test_feature_2"]
-        test_feature_3 = request.form["test_feature_3"]
-        features = [test_feature_1, test_feature_2, test_feature_3]
+# features looks like: [track_popularity, danceability, energy, key, loudness, mode, speechiness, acousticness, liveness, valence, tempo, duration_ms]
+        track_popularity = request.form["track_popularity"] 
+        danceability = request.form["danceability"]
+        energy = request.form["energy"]
+        key = request.form["key"]
+        loudness = request.form["loudness"]
+        mode = request.form["mode"]
+        speechiness = request.form["speechiness"]
+        acousticness = request.form["acousticness"]
+        liveness = request.form["liveness"]
+        valence = request.form["valence"]
+        tempo = request.form["tempo"]
+        duration_ms = request.form["duration_ms"]
+        features = [track_popularity, danceability, energy, key, loudness, mode, speechiness, acousticness, liveness, valence, tempo, duration_ms]
 
         #If there is an empty string, raise a KeyError anyways.
         for feature in features:
@@ -63,7 +78,7 @@ def receive_form_data():
                 return render_error("Submitted incomplete form!")
 
         #We have the features; send them to the model, and display the resulting genre.
-        output_lines = ["test_1", "test_2"]
+        output_lines = ["test_1", "test_2", f"test {features}"]
         return render_template("index.html", features_available=True, features=features, output_available=True, output_lines=output_lines)
 
 from flask import url_for
